1a5491371f81b3a9b84230bd279dbacd835f14a0
Justin <jjpeterson90@hotmail.com>
2022-05-31 11:44:22 -0500

    initial completion

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,27 @@
 class GuessingGame():
+    def __init__(self, answer):
+        self.answer = answer
+        self.last_guess = None
+    
+    def solved(self):
+        print(self.last_guess == self.answer)
+    
+    def guess(self, user_guess):
+        if self.answer == self.last_guess:
+            print(f"You already solved it. The answer was {self.last_guess}!")
+        elif user_guess < self.answer:
+            print("Low")
+        elif user_guess > self.answer:
+            print("High")
+        elif user_guess == self.answer:
+            print("Correct!")
+        self.last_guess = user_guess
+        
+game = GuessingGame(10)
+
+game.guess(5)
+game.guess(20)
+game.solved()
+game.guess(10)
+game.solved()
+game.guess(4)
