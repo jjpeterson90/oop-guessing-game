02a7257ed87bb3f039760a5373288731ed4f5f0a
Justin <jjpeterson90@hotmail.com>
2022-05-31 11:58:28 -0500

    minor changes to fn outputs

--- guessing_game.py
+++ guessing_game.py
@@ -4,24 +4,26 @@ class GuessingGame():
         self.last_guess = None
     
     def solved(self):
-        print(self.last_guess == self.answer)
+        return self.last_guess == self.answer
     
     def guess(self, user_guess):
         if self.answer == self.last_guess:
-            print(f"You already solved it. The answer was {self.last_guess}!")
+            return f"You already solved it. The answer was {self.last_guess}!"
         elif user_guess < self.answer:
-            print("Low")
+            self.last_guess = user_guess
+            return "Low"
         elif user_guess > self.answer:
-            print("High")
+            self.last_guess = user_guess
+            return "High"
         elif user_guess == self.answer:
-            print("Correct!")
-        self.last_guess = user_guess
+            self.last_guess = user_guess
+            return "Correct!"
         
 game = GuessingGame(10)
 
-game.guess(5)
-game.guess(20)
-game.solved()
-game.guess(10)
-game.solved()
-game.guess(4)
+print(game.guess(5))
+print(game.guess(20))
+print(game.solved())
+print(game.guess(10))
+print(game.solved())
+print(game.guess(4))
